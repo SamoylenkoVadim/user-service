1037c4c2cc82f9476d40ea90d0719462bd879916
Vadim Samoilenko <vsam26@yandex.ru>
2023-05-16 23:44:34 +0200

    api adaptation for index, create, delete

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
@@ -20,11 +21,17 @@ class Email(db.Model):
     mail = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
 
+    def __repr__(self):
+        return self.mail
+
 
 class PhoneNumber(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
+    def __repr__(self):
+        return self.number
 
 
 with app.app_context():
@@ -39,53 +46,91 @@ def index():
         emails = db.session.query(Email).filter_by(user_id=user.id).all()
         phone_numbers = db.session.query(PhoneNumber).filter_by(user_id=user.id).all()
         users_data.append({
-            'user': user,
-            'emails': emails,
-            'phone_numbers': phone_numbers
+            'id': user.id,
+            'firstName': user.firstName,
+            'lastName': user.lastName,
+            'emails': [str(email) for email in emails],
+            'phone_numbers': [str(phone_number) for phone_number in phone_numbers],
         })
 
-    return render_template('index.html', users=users_data)
+    if request.headers.get("Accept") == "application/json":
+        return jsonify(users_data=users_data)
+
+    return render_template('index.html', users_data=users_data)
 
 
 @app.route("/create", methods=['GET', 'POST'])
 def create():
+    is_api_request = request.headers.get("Accept") == "application/json" or request.is_json
+
+    if request.method == "GET":
+        if is_api_request:
+            return jsonify(status="Method Not Allowed"), 405
+        else:
+            return render_template('create.html')
+
     if request.method == "POST":
-        try:
-            firstName = request.form["firstName"].strip()
-            lastName = request.form["lastName"].strip()
-            mail = request.form["mail"].strip()
-            number = request.form["number"].strip()
-        except Exception as e:
-            return f'Bad request: {e}', 400
+        if is_api_request:
+            data = request.json
+            firstName = data.get("firstName", "").strip() or None
+            lastName = data.get("lastName", "").strip() or None
+            mail = data.get("mail", "").strip() or None
+            number = data.get("number", "").strip() or None
+        else:
+            firstName = request.form.get("firstName", "").strip() or None
+            lastName = request.form.get("lastName", "").strip() or None
+            mail = request.form.get("mail", "").strip() or None
+            number = request.form.get("number", "").strip() or None
+
+        if firstName is None or lastName is None:
+            if is_api_request:
+                return jsonify(status="Bad request: firstName and lastName are required"), 400
+            else:
+                return 'Bad request: firstName and lastName are required', 400
 
         try:
             user = User(firstName=firstName, lastName=lastName)
-            email = Email(mail=mail)
-            phone_number = PhoneNumber(number=number)
+            if mail:
+                email = Email(mail=mail)
+                user.emails.extend([email])
 
-            user.emails.extend([email])
-            user.phoneNumbers.extend([phone_number])
+            if number:
+                phone_number = PhoneNumber(number=number)
+                user.phoneNumbers.extend([phone_number])
 
             db.session.add(user)
             db.session.commit()
 
-            return redirect("/")
+            if is_api_request:
+                return jsonify(status="Created"), 201
+            else:
+                return redirect("/")
+
         except Exception as e:
-            return f'Internal Server Error: {e}', 500
-    else:
-        return render_template('create.html')
+            if is_api_request:
+                return jsonify(status=f'Internal Server Error: {e}'), 500
+            else:
+                return f'Internal Server Error: {e}', 500
 
 
 @app.route("/delete/<user_id>", methods=["GET", "DELETE"])
 def delete(user_id):
+    is_api_request = request.headers.get("Accept") == "application/json" or request.is_json
     try:
         user = db.session.get(User, user_id)
         if user:
             db.session.delete(user)
             db.session.commit()
     except Exception as e:
-        return f'Internal Server Error: {e}', 500
-    return redirect("/")
+        if is_api_request:
+            return jsonify(status=f'Internal Server Error: {e}'), 500
+        else:
+            return f'Internal Server Error: {e}', 500
+
+    if is_api_request:
+        return jsonify(status='Record deleted successfully'), 204
+    else:
+        return redirect("/")
 
 
 @app.route("/edit/<user_id>", methods=["GET", "PUT", "POST"])
@@ -140,4 +185,4 @@ def edit(user_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
